a9ecfaed76d6f0884a44ae5e206a009e062d645f
Steve <sjd734312@gmail.com>
2023-06-03 16:05:55 -0400

    loop lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+
+const names = ['Trevor', 'Carty', 'Jay'];
+
+function writeCards(names){
+    let messages = [];
+    for (let i = 0; i < names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return messages;
+}
+
+function countDown(){
+    let i = 0
+    while (i < 11){
+        console.log(i)
+        i++
+    }
+}
